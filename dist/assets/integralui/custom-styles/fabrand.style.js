6021e055662a30976ed67a27214a7759b712a17e
fwcloud <info@soltecsis.com>
2026-04-22 10:20:43 +0000

    Deploying to devel from @ soltecsis/fwcloud-ui-src@155529dd820fcef99f682a03575944ddccf76a4d 🚀

--- dist/assets/integralui/custom-styles/fabrand.style.js
+++ dist/assets/integralui/custom-styles/fabrand.style.js
@@ -1,11 +1,9 @@
 import { css } from 'app/integralui/external/lit-element';
 
 export const FaBrandStyle = css`
-    @import url('node_modules/@fortawesome/fontawesome-free/css/all.css');
-
     .fa-brands {
         display: inline-block;
-        font-family: 'Font Awesome 5 Brands';
+        font-family: 'Font Awesome 7 Brands';
         font-weight: normal;
         font-style: normal;
         font-size: inherit;
